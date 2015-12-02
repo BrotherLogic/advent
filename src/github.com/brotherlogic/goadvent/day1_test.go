92d1db69dfe1f46f124903a8ed9cbb7a99c16e7a
Simon Tucker <brotherlogic@gmail.com>
2015-12-01 20:24:05 -0800

    Completes test and output for second part of day 1 of the advent test.
    Added failing test
    
    Added support, tests still failing
    
    Fixed out code to return correct answer

--- src/github.com/brotherlogic/goadvent/day1_test.go
+++ src/github.com/brotherlogic/goadvent/day1_test.go
@@ -2,6 +2,23 @@ package main
 
 import "testing"
 
+func TestDayP2(t *testing.T) {
+	cases := []struct {
+		in string
+		want int
+	} {
+		{")", 1},
+		{"()())", 5},
+	}
+
+	for _, c := range cases {
+		got := ComputeF1(c.in)
+		if got != c.want {
+			t.Errorf("Spec(%q) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
+
 func TestDay1(t *testing.T) {
 
 	cases := []struct {
