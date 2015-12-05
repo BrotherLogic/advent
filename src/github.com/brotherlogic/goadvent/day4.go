ed42d45bfaff58d5836a8d2d97da903b2533a61b
Simon Tucker <brotherlogic@gmail.com>
2015-12-04 17:29:47 -0800

    Solved Day 4 Part 1. This clsoes #16.
    Updated test for solving day4 p1
    
    Solved for hash function

--- src/github.com/brotherlogic/goadvent/day4.go
+++ src/github.com/brotherlogic/goadvent/day4.go
@@ -1,9 +1,26 @@
 package main
 
+import "crypto/md5"
+import "encoding/hex"
 import "fmt"
+import "strconv"
+import "strings"
 
 func SolveHash(str string) int {
-	return 0
+	value := 0
+	found := false
+
+	for !found {
+		value++
+		data := []byte(str + strconv.Itoa(value))
+		hash := md5.New()
+		hash.Write(data)
+		md5v := hex.EncodeToString(hash.Sum(nil))
+		if strings.HasPrefix(md5v,"00000") {
+			found = true
+		}
+	}
+	return value;
 }
 
 func dayfour() {
