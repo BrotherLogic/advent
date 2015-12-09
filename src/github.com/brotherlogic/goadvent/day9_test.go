7883591995d18d323359c79526d5f75a3f4e2885
Simon Tucker <brotherlogic@gmail.com>
2015-12-08 21:54:42 -0800

    Test Passing

---
+++ src/github.com/brotherlogic/goadvent/day9_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestDay9P1(t *testing.T) {
+
+	arr := [][]int {}
+	for i := 0 ; i < 3 ; i++ {
+		row := make([]int,3)
+		arr = append(arr,row)
+	}
+
+	arr[0][1] = 464
+	arr[0][2] = 518
+	arr[1][0] = 464
+	arr[1][2] = 141
+	arr[2][0] = 518
+	arr[2][1] = 141
+	
+	answer := SearchMap(arr)
+		
+	if answer != 605 {
+		t.Errorf("%d, want %d", answer, 605)
+	}
+}
