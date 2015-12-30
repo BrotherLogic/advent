8bc3b6505732444dcd557d611b67747cf3d8ef8c
Simon Tucker <brotherlogic@gmail.com>
2015-12-29 21:56:13 -0800

    Cleared test output

--- src/github.com/brotherlogic/goadvent/day18_test.go
+++ src/github.com/brotherlogic/goadvent/day18_test.go
@@ -18,9 +18,7 @@ func TestDay18P1(t *testing.T) {
 		t.Errorf("Wrong init count %v\n",CountArr(initArr))
 	}
 
-	fmt.Printf("%v,%v\n", ComputeNeighbors(initArr,0,0),initArr)
 	following1 := Iterate(initArr)
-	fmt.Printf("11,%v, %v\n",CountArr(following1),following1)
 	following2 := Iterate(following1)
 	following3 := Iterate(following2)
 	following4 := Iterate(following3)
