9bd4eaee08f352f223ea663b5c23c65ac99c9815
Simon Tucker <brotherlogic@gmail.com>
2020-11-13 17:45:37 -0800

    Adjusted test to pass

--- 2019/day2.go
+++ 2019/day2.go
@@ -1,5 +1,15 @@
 package a2019
 
 func runOpCode1(program []int) int {
-	return 0
+	pointer := 0
+	for program[pointer] != 99 {
+		if program[pointer] == 1 {
+			program[program[pointer+3]] = program[program[pointer+1]] + program[program[pointer+2]]
+		} else if program[pointer] == 2 {
+			program[program[pointer+3]] = program[program[pointer+1]] * program[program[pointer+2]]
+		}
+
+		pointer += 4
+	}
+	return program[0]
 }
--- 2019/day2_test.go
+++ 2019/day2_test.go
@@ -2,9 +2,26 @@ package a2019
 
 import "testing"
 
+type test struct {
+	input  []int
+	output int
+}
+
+var tests = []test{
+	{input: []int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50}, output: 3500},
+	{input: []int{1, 0, 0, 0, 99}, output: 2},
+	{input: []int{2, 3, 0, 3, 99}, output: 2},
+	{input: []int{2, 4, 4, 5, 99, 0}, output: 2},
+	{input: []int{1, 1, 1, 4, 99, 5, 6, 0, 99}, output: 30},
+}
+
 func TestOpCode1(t *testing.T) {
-	result := runOpCode1([]int{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50})
-	if result != 3500 {
-		t.Errorf("Bad run: %v, should be 3500", result)
+	for _, test := range tests {
+		ti := make([]int, len(test.input))
+		copy(ti, test.input)
+		result := runOpCode1(test.input)
+		if result != test.output {
+			t.Errorf("Bad run: %v led to %v, should be %v", ti, result, test.output)
+		}
 	}
 }
